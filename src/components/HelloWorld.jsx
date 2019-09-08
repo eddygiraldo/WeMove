dafa9cdf3fc20e8d0fa8ddc1a73b44d8e1fa676e
Eddy Giraldo <eddygiraldo@gmail.com>
2019-09-08 14:18:03 -0500

    first login form

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -2,7 +2,55 @@ import React from 'react';
 import '../assets/styles/App.scss';
 
 const HelloWorld = () => (
-  <h1>Hola Mundo</h1>
+  <div class="login">
+    <div class="login-message">
+      <img src="" clas="login-logo" alt=""/>
+      <h2>Bienvenido de nuevo</h2>
+      <p>
+        Si ya tienes una cuenta, sólo inicia sesión.
+      </p>
+
+      <button class="btn btn-transparent">
+        Ingresar
+      </button>
+    </div>
+    <div class="login-form">
+      <h2>
+        Crea una cuenta gratis
+      </h2>
+      <h3>
+        Registrate usando redes sociales
+      </h3>
+      <div class="social-media">
+        <span>Facebook</span>
+        <span>Twitter</span>
+      </div>
+      <h3>O usa un correo para el registro</h3>
+      <form action="#" class="signup-form">
+        <input 
+          type="text"
+          class="input-generic"
+          placeholder="Nombre"
+        />
+
+        <input 
+          type="email"
+          class="input-generic"
+          placeholder="Correo"
+        />
+
+        <input
+          type="password"
+          class="input-generic"
+          placeholder="Contraseña"
+        />
+
+        <button class="btn btn-submit">
+          Registrarse
+        </button>
+      </form>
+    </div>
+  </div>
 )
 
 
