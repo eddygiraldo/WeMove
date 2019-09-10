ee421a8dd1d431ad2f7c19bf0dd5455eddc868b2
Eddy Alexander Giraldo Pinilla <xcbegira@intcomex.com>
2019-09-09 21:07:09 -0500

    init login form

--- src/Routes/App.js
+++ src/Routes/App.js
@@ -6,7 +6,6 @@ import Login from '../containers/Login';
 const App = () => (
   <BrowserRouter>
     <Route exact path="/" component={Home} />
-    <Route exact path="/login" component={Login} />
   </BrowserRouter>
 );
 
