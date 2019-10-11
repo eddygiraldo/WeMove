e5ad6010a7755205211d5e643d5a16c0f6af37b1
Eddy Alexander Giraldo Pinilla <xcbegira@intcomex.com>
2019-10-10 22:00:55 -0500

    add logo in login pages

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -2,50 +2,61 @@ import React from 'react';
 import '../assets/styles/components/Login.scss';
 import FacebookIcon from '../assets/static/facebook-icon.svg';
 import GmailIcon from '../assets/static/gmail-icon.svg';
+import LogoBlack from '../assets/static/logo-black.png';
 
 const Login = () => (
-  <div className="login">
-    <div className="login-form">
-      <h2>
-        Ingrese a su cuenta
+  <div>
+
+    <a href="/" className="login-logo">
+      <img
+        src={LogoBlack}
+        alt="Logo"
+        className="header-logo"
+      />
+    </a>
+
+    <div className="login">
+      <div className="login-form">
+        <h2>
+          Ingrese a su cuenta
       </h2>
-      <h3>
-        Ingresa usando redes sociales
+        <h3>
+          Ingresa usando redes sociales
       </h3>
-      <div className="social-media">
-        <span><img src={FacebookIcon} alt="" width="30px" /></span>
-        <span><img src={GmailIcon} alt="" width="30px" /></span>
-      </div>
-      <h3>O</h3>
-      <form action="#" className="signup-form">
-        <input
-          type="email"
-          class="input-generic"
-          placeholder="Correo"
-        />
+        <div className="social-media">
+          <span><img src={FacebookIcon} alt="" width="30px" /></span>
+          <span><img src={GmailIcon} alt="" width="30px" /></span>
+        </div>
+        <h3>O</h3>
+        <form action="#" className="signup-form">
+          <input
+            type="email"
+            class="input-generic"
+            placeholder="Correo"
+          />
 
-        <input
-          type="password"
-          class="input-generic"
-          placeholder="Contraseña"
-        />
+          <input
+            type="password"
+            class="input-generic"
+            placeholder="Contraseña"
+          />
 
-        <button class="btn btn-submit">
-          Ingresar
+          <button class="btn btn-submit">
+            Ingresar
         </button>
-      </form>
-    </div>
+        </form>
+      </div>
 
-    <div className="login-message">
-      <a href="/" className="login-logo"><img src="" alt="" />Volver</a>
-
-      <h2>¿Nuevo aquí?</h2>
-      <p>
-        Registrate y descubre gran cantidad de grandes oportunidades.
+      <div className="login-message">
+        <h2>¿Nuevo aquí?</h2>
+        <p>
+          Registrate y descubre gran cantidad de grandes oportunidades.
       </p>
-      
-      <a className="btn btn-transparent" href="/register">Registrarse</a>
+
+        <a className="btn btn-transparent" href="/register">Registrarse</a>
+      </div>
     </div>
+    
   </div>
 )
 
--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -2,54 +2,65 @@ import React from 'react';
 import '../assets/styles/components/Login.scss';
 import FacebookIcon from '../assets/static/facebook-icon.svg';
 import GmailIcon from '../assets/static/gmail-icon.svg';
+import LogoBlack from '../assets/static/logo-black.png';
 
 const Register = () => (
-  <div className="login">
-    <div className="login-message">
-      <a href="/" className="login-logo"><img src="" alt="" />Volver</a>
+  <div>
 
-      <h2>Bienvenido de nuevo</h2>
-      <p>
-        Si ya tienes una cuenta, sólo inicia sesión.
+    <a href="/" className="login-logo">
+      <img
+        src={LogoBlack}
+        alt="Logo"
+        className="header-logo"
+      />
+    </a>
+
+    <div className="login">
+      <div className="login-message">
+
+        <h2>Bienvenido de nuevo</h2>
+        <p>
+          Si ya tienes una cuenta, sólo inicia sesión.
       </p>
 
-      <a href="/login" className="btn btn-transparent">Ingresar</a>
-    </div>
-    <div className="login-form">
-      <h2>
-        Crea una cuenta gratis
-      </h2>
-      <h3>
-        Registrate usando redes sociales
-      </h3>
-      <div className="social-media">
-        <span><img src={FacebookIcon} alt="" width="30px" /></span>
-        <span><img src={GmailIcon} alt="" width="30px" /></span>
+        <a href="/login" className="btn btn-transparent">Ingresar</a>
       </div>
-      <h3>O usa un correo para el registro</h3>
-      <form action="#" className="signup-form">
-        <input
-          type="text"
-          className="input-generic"
-          placeholder="Nombre"
-        />
+      <div className="login-form">
+        <h2>
+          Crea una cuenta gratis
+      </h2>
+        <h3>
+          Registrate usando redes sociales
+      </h3>
+        <div className="social-media">
+          <span><img src={FacebookIcon} alt="" width="30px" /></span>
+          <span><img src={GmailIcon} alt="" width="30px" /></span>
+        </div>
+        <h3>O usa un correo para el registro</h3>
+        <form action="#" className="signup-form">
+          <input
+            type="text"
+            className="input-generic"
+            placeholder="Nombre"
+          />
 
-        <input
-          type="email"
-          class="input-generic"
-          placeholder="Correo"
-        />
+          <input
+            type="email"
+            class="input-generic"
+            placeholder="Correo"
+          />
 
-        <input
-          type="password"
-          class="input-generic"
-          placeholder="Contraseña"
-        />
+          <input
+            type="password"
+            class="input-generic"
+            placeholder="Contraseña"
+          />
 
-        <button class="btn btn-submit">
-          Registrarse
+          <button class="btn btn-submit">
+            Registrarse
         </button>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
 )
